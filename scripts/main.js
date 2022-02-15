0b19dabb1dd399c2e49fdf51fc513e115932b301
Szczuply <91548945+Szczuply@users.noreply.github.com>
2022-02-15 14:20:27 +0100

    now for phones

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,35 @@
 const abilitiesButton = document.querySelector('#abilitiesButton')
+const homeButton = document.querySelector('#homeButton')
+const aboutButton = document.querySelector('#aboutButton')
+const portfolioButton = document.querySelector('#portfolioButton')
+const contactButton = document.querySelector('#contactButton')
+const activator = document.querySelector('#nav-barActivator')
+const NavBar = document.querySelector('.nav-bar')
+
+
+homeButton.addEventListener(`click`, () =>{
+    document.querySelector('.container .header').scrollIntoView();
+    NavBar.classList.toggle('open')
+})
+
+aboutButton.addEventListener(`click`, () =>{
+    document.querySelector('.about').scrollIntoView();
+    NavBar.classList.toggle('open')
+})
+
+portfolioButton.addEventListener(`click`, () =>{
+    document.querySelector('.portfolio').scrollIntoView();
+    NavBar.classList.toggle('open')
+})
+
+contactButton.addEventListener(`click`, () =>{
+    document.querySelector('.contact').scrollIntoView();
+    NavBar.classList.toggle('open')
+})
+
+activator.addEventListener('click', ()=>{
+    NavBar.classList.toggle('open')
+})
 
 
 
@@ -11,3 +42,4 @@ abilitiesButton.addEventListener('click', () => {
 function DarkModeToggle(){
     document.body.classList.toggle("dark-mode")
 }
+
